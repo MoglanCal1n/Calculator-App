827a4e8915faf5b8bb83419d6bb104ac40acf336
Moglan Calin-Stefan <150551976+MoglanCal1n@users.noreply.github.com>
2024-01-27 00:05:17 +0200

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,20 @@
+//Salut sunt calin si fac calculator
+
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+      display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Sugi pula";
+    }
+}
